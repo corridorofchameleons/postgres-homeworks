c0dd01b28865399928f005f6c642a054979271ee
speedfreak <gdramze@gmail.com>
2024-03-25 22:10:07 +0300

    Initial

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,12 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_NAME = os.getenv('DB_NAME')
