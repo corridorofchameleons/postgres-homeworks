0c6ac65037fa616e84dad6ccf28be56940dfc4d5
speedfreak <gdramze@gmail.com>
2024-03-25 22:38:52 +0300

    Create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers_data
+	(
+		customer_id CHAR(5) PRIMARY KEY,
+		company_name CHAR(255),
+		contact_name CHAR(50)
+	);
+
+CREATE TABLE employees_data
+	(
+		employee_id SERIAL PRIMARY KEY,
+		first_name CHAR(15),
+		last_name CHAR(15),
+		title CHAR(50), -- здесь конечно же нужен ENUM
+		birth_date DATE,
+		notes TEXT
+	);
+
+CREATE TABLE orders_data
+	(
+		order_id INT PRIMARY KEY,
+		customer_id CHAR(5) REFERENCES customers_data(customer_id),
+		employee_id INT REFERENCES employees_data(employee_id),
+		order_date DATE,
+		ship_city CHAR(20)
+	);
